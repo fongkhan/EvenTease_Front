9229baafee16174b4f22d531bf727ca916249057
LouisDelestre <louis.delestre@gmail.com>
2022-02-24 15:06:50 +0100

    Ajout Home + Help

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { HelpComponent } from './help/help.component';
+import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {component: ConnexionComponent, path:"login"},
+  {component: HomeComponent, path:"home"},
+  {component: HelpComponent, path:"help"}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { HomeComponent } from './home/home.component';
+import { HelpComponent } from './help/help.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConnexionComponent
+    ConnexionComponent,
+   
+    HomeComponent,
+         HelpComponent
   ],
   imports: [
     BrowserModule,
