ff61ba49d6b943f932ef3c37f6cbbe42949d51a6
MaxPea <max.pean807@gmail.com>
2022-02-28 13:51:28 +0100

    prise en compte de l'id de session pour la création d'event74

--- src/app/event-creation/event-creation.component.ts
+++ src/app/event-creation/event-creation.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,21 +12,22 @@ export class EventCreationComponent implements OnInit {
 
   event: any;
   msgErr = '';
+  user:any;
 
-
-  constructor(private http: HttpClient, private route: Router) { }
+  constructor(private http: HttpClient, private route: Router,private auth: AuthService) { }
 
   ngOnInit(): void {}
 
-  
 
   CreateEvent(event:any){
+    this.user = this.auth.getUserConnect();
+    event["organizer"]=this.user;
+    console.log(this.user);
     console.log("les datas du formulaire",event);
     this.http.post('http://localhost:8182/event/create',event).subscribe({
       next: (data)=> {
         console.log("ok");
-        
-        
+        this.route.navigateByUrl('auth-user-home');
       },
       error: (err)=>{console.log(err)}
     });
