c1fb416c8cfaf554a31ec61da7e7de6ae5ca2993
LouisDelestre <louis.delestre@gmail.com>
2022-03-01 13:32:06 +0100

    modif edit profil

--- src/app/edit-profil/edit-profil.component.ts
+++ src/app/edit-profil/edit-profil.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../service/auth.service';
 
 @Component({
@@ -8,7 +10,10 @@ import { AuthService } from '../service/auth.service';
 })
 export class EditProfilComponent implements OnInit {
 
-  constructor(public auth: AuthService) { }
+  // user:any;
+  msgErr = '';
+
+  constructor(private http: HttpClient, public auth: AuthService, private route: Router,) { }
   
   ngOnInit(): void {
   }
@@ -16,6 +21,43 @@ export class EditProfilComponent implements OnInit {
   get user(): any {
     return this.auth.getUserConnect();
     }
+
+/*
+    EditProfil(user:any){
+      console.log("les datas du formulaire",user);
+      this.http.post('http://localhost:8182/edit-profil',user).subscribe({
+        next: (data)=> {
+          this.user = data; 
+          if(this.user != null) {
+            console.log('ok');
+
+            // ajouter la comande qui envoie le JSON
+
+
+
+            this.route.navigateByUrl('profil-perso');
+            
+          }
+          else{
+            // recuperer la donnée de base et l'envoyer avec le form / pas laisser de champ vide;
+
+
+            this.route.navigateByUrl('profil-perso');
+
+
+          }
+        },
+        error: (err)=>{console.log(err)}
+      });
+    }
+
+
+
+
+
+*/
+
+
 
 }
 
