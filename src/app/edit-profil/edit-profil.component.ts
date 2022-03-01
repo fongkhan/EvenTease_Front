ed43fe0db5aa039ef86e260ab1e4f6a2e16ed09f
LouisDelestre <louis.delestre@gmail.com>
2022-03-01 09:23:35 +0100

    test

--- src/app/edit-profil/edit-profil.component.ts
+++ src/app/edit-profil/edit-profil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-edit-profil',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfilComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public auth: AuthService) { }
+  
   ngOnInit(): void {
   }
 
+  get user(): any {
+    return this.auth.getUserConnect();
+    }
+
 }
+
+  
+
+
+
