e657e72f78096377036347c2b1425f57e9686d34
MaxPea <max.pean807@gmail.com>
2022-03-02 11:53:56 +0100

    page event finie (manque bouton requete participer)

--- src/app/event-public/event-public.component.ts
+++ src/app/event-public/event-public.component.ts
@@ -10,28 +10,57 @@ import { EventDetailsService } from '../service/event-details.service';
   styleUrls: ['./event-public.component.css']
 })
 export class EventPublicComponent implements OnInit {
-  id:any ;
-  event:any;
-  msgErr ='';
+  id: any;
+  
+  event: any;
+  msgErr = '';
+  participants: any;
 
-  constructor(private http:HttpClient, private route: Router, public eventDet: EventDetailsService,public auth: AuthService) { }
+  constructor(private http: HttpClient, private route: Router, public eventDet: EventDetailsService, public auth: AuthService) { }
 
   ngOnInit(): void {
+
     this.id = this.eventDet.getEventId();
-    this.http.post('http://localhost:8182/eventid',this.id).subscribe({
-      next: (data)=> {
-        this.event = data; 
-        if(this.event!= null) {
+    this.http.post('http://localhost:8182/eventid', this.id).subscribe({
+      next: (data) => {
+        this.event = data;
+        if (this.event != null) {
           console.log(this.event)
         }
-        else{
-          this.msgErr ='No event to show';
+        else {
+          this.msgErr = 'No event to show';
         }
       },
-        error: (err)=>{console.log(err)}
-      });
+      error: (err) => { console.log(err) }
+    });
+
+    this.http.post('http://localhost:8182/event/participant/event',this.id).subscribe({
+      next: (data2) => {
+        this.participants = data2;
+        if (this.participants != null) {
+          console.log(this.participants)
+        }
+        else {
+          this.msgErr = 'No participants';
+        }
+      },
+      error: (err) => { console.log(err) }
+    });
+
+
+  }
+  isPublicCheck() {
+
+    if (this.event["isPublic"]) {
+      return true;
+
+    } else {
+      return false;
     }
   }
+}
 
-  
+
+
+
 
